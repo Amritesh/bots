d51d944c778a6ae37ad3c689097891ab97d7326e
Amritesh <thenectorgod@gmail.com>
2020-08-22 01:56:12 +0530

    Logs profileLink and cap on totalComments + delay added

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const fs = require('fs');
     const halt = argv._.includes('halt');
     let visitedPosts = [];
     fs.readFile(config.logFile, 'utf-8', function (err, content) {
-        visitedPosts = content.split('\n');
+        visitedPosts = content.split('\n').map(line=>line !== "" && JSON.parse(line)).filter(Boolean);
     });
     if (run) {
         const browser = await chromium.puppeteer.launch({
             executablePath: await chromium.executablePath,
-            headless: chromium.headless
+            headless: chromium.headless,
+            debugger: true
         });
         try {
             const page = (await browser.pages())[0];
@@ -34,15 +35,30 @@ const fs = require('fs');
                 const allLinks = await page.$$eval('a', as => as.map(a => a.href));
                 const postLinkRE = new RegExp(config.postLink, "g");
                 let postLinks = allLinks.filter(link => link.match(postLinkRE));
+                let count = 0;
                 for (postLink of postLinks) {
-                    if(!visitedPosts.includes(postLink)){
+                    try {
                         await page.goto(postLink, { waitUntil: 'networkidle2' });
-                        const randomComment = comments[Math.floor(Math.random() * comments.length)];
-                        await page.type("textarea", randomComment);
-                        const [button] = await page.$x("//button[contains(., 'Post')]");
-                        // button && await button.click();
-                        fs.appendFile(config.logFile,postLink+"\n",()=>{});
+                        const [profileRef] = await page.$x("//button[contains(., 'Follow')]/../../div[1]/span/a");
+                        const profileLink = await page.evaluate(el => el.href, profileRef);
+                        await page.waitFor(Math.random()*1000);
+                        if(!visitedPosts.find(post=>post.profileLink === profileLink)){
+                            const post = {postLink, profileLink }
+                            const randomComment = comments[Math.floor(Math.random() * comments.length)];
+                            await page.type("textarea", randomComment);
+                            const [button] = await page.$x("//button[contains(., 'Post')]");
+                            await page.waitFor(Math.random()*10000);
+                            // button && await button.click();
+                            fs.appendFile(config.logFile,JSON.stringify(post)+"\n",()=>{});   
+                            count++;
+                        } else {
+                            console.log(`skipped ${profileLink}`);
+                        }
+                    } catch (e) {
+                        console.log(e, postLink);
                     }
+                    if(count >= config.totalComments)
+                        break;
                 }
             }
         } catch (error) {
