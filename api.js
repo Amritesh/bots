01d1120fd55371322de3dc4c9ecf50e1c289d5ca
Amritesh <thenectorgod@gmail.com>
2020-08-22 16:29:00 +0530

    get links with scroll

---
+++ api.js
@@ -0,0 +1,21 @@
+exports.autoScroll = async (page, scrollCount) => {
+    return await page.evaluate(async ({scrollCount}) => {
+        let allLinks = await new Promise((resolve, reject) => {
+            let count = 0;
+            let distance = 1000;
+            let allLinks = [];
+            const timer = setInterval(() => {
+                window.scrollBy(0, distance);
+                count++;
+                const htmlCollection = document.getElementsByTagName('a')
+                const links = Array.prototype.slice.call( htmlCollection ).map(b=>b.href);
+                allLinks = Array.from(new Set([...allLinks, ...links]));
+                if(scrollCount<count){
+                    clearInterval(timer);
+                    resolve(allLinks);
+                }
+            }, Math.random()*2000+2000);
+        });
+        return allLinks;
+    }, {scrollCount});
+}
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const fs = require('fs');
         const browser = await chromium.puppeteer.launch({
             executablePath: await chromium.executablePath,
             headless: chromium.headless,
-            debugger: true
+            devtools: true
         });
         try {
             const page = (await browser.pages())[0];
@@ -32,27 +32,33 @@ const fs = require('fs');
             for (tag of config.tags) {
                 const tagspage = config.tagurl + tag;
                 await page.goto(tagspage, { waitUntil: 'networkidle2' });
-                const allLinks = await page.$$eval('a', as => as.map(a => a.href));
+                const allLinks = await api.autoScroll(page,config.scrollCount);
                 const postLinkRE = new RegExp(config.postLink, "g");
                 let postLinks = allLinks.filter(link => link.match(postLinkRE));
                 let count = 0;
+                console.log("total Posts", postLinks.length);
                 for (postLink of postLinks) {
                     try {
-                        await page.goto(postLink, { waitUntil: 'networkidle2' });
-                        const [profileRef] = await page.$x("//button[contains(., 'Follow')]/../../div[1]/span/a");
-                        const profileLink = await page.evaluate(el => el.href, profileRef);
-                        await page.waitFor(Math.random()*1000);
-                        if(!visitedPosts.find(post=>post.profileLink === profileLink)){
-                            const post = {postLink, profileLink }
-                            const randomComment = comments[Math.floor(Math.random() * comments.length)];
-                            await page.type("textarea", randomComment);
-                            const [button] = await page.$x("//button[contains(., 'Post')]");
-                            await page.waitFor(Math.random()*10000);
-                            // button && await button.click();
-                            fs.appendFile(config.logFile,JSON.stringify(post)+"\n",()=>{});   
-                            count++;
+                        if(!visitedPosts.find(post=>post.postLink === postLink)) {
+                            await page.goto(postLink, { waitUntil: 'networkidle2' });
+                            const [profileRef] = await page.$x("//button[contains(., 'Follow')]/../../div[1]/span/a");
+                            const profileLink = await page.evaluate(el => el.href, profileRef);
+                            await page.waitFor(Math.random()*1000);
+                            if(!visitedPosts.find(post=>post.profileLink === profileLink)){
+                                const post = {postLink, profileLink }
+                                const randomComment = comments[Math.floor(Math.random() * comments.length)];
+                                await page.type("textarea", randomComment);
+                                const [button] = await page.$x("//button[contains(., 'Post')]");
+                                await page.waitFor(Math.random()*10000);
+                                button && await button.click();
+                                visitedPosts.push(post);
+                                fs.appendFile(config.logFile,JSON.stringify(post)+"\n",()=>{});   
+                                count++;
+                            } else {
+                                console.log(`skipped ${profileLink}`);
+                            }
                         } else {
-                            console.log(`skipped ${profileLink}`);
+                            console.log(`skipped ${postLink}`);
                         }
                     } catch (e) {
                         console.log(e, postLink);
